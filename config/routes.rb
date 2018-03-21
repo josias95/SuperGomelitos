69ffa7bc42b1d64161dd96b45af4bd4f7544686f
jdavidt <ingsotfwareuniquindio@gmail.com>
2018-03-20 23:53:09 -0500

    agregado controlador para pagina de inicio

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #ruta principal
-  root to: "home#index"
+  root to: "homes#index"
 end
