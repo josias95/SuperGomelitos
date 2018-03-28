d45d811e38cb00101ae5e18f85bc4f71d8e0e80b
jdavidt <ingsotfwareuniquindio@gmail.com>
2018-03-28 17:51:48 -0500

    se puede editar usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_devise_params, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
 
-  def configure_devise_params
-  	devise_parameter_sanitizer.permit(:sign_up) do |user|
-  		user.permit(:name, :email, :password, :password_confirmation)
-  	end
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  	devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
-
 end
