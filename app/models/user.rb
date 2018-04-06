05abc70cf2bb05471c0073978e8160bef04b737c
jdavidt <ingsotfwareuniquindio@gmail.com>
2018-04-06 16:55:21 -0500

    se agrego en el modelo user: un usuario puede tener 1 ó varios eventos

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100"}
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
-
+  has_many :events
 end
