4e0574c78005f94183078555e163a5cc83c89257
JohnCarlos95 <johnkchein3@gmail.com>
2018-04-06 04:02:50 -0500

    se implemento el street view de google maps

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,5 +13,6 @@ module SuperGomelitos
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Setup global parameters
   end
 end
