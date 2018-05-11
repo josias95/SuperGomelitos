a71864fd760398b702713adca8fc0a0b7c7bb4da
anonymus95 <ingsotfwareuniquindio@gmail.com>
2018-05-11 15:46:04 -0500

    se valida que el valor de un servicio no sea negativo

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,8 @@ class Service < ApplicationRecord
 
 	validates :nombre, presence: true
 	validates :descripcion, presence: true
-	validates :valor, presence: true
+	validates_numericality_of :valor, :greater_than => -1
+
 	enum categoria: [:Bebes, :Niños, :Adultos]
 	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100"}
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
