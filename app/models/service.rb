ba6d631c1a3b61a91f937a2538c4c0956bb22544
anonymus95 <ingsotfwareuniquindio@gmail.com>
2018-05-30 09:08:59 -0500

    se actualizaron las vistas de servicios y eventos

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,6 +5,4 @@ class Service < ApplicationRecord
 	validates_numericality_of :valor, :greater_than => -1
 
 	enum categoria: [:Bebes, :Niños, :Adultos]
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100"}
-	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
